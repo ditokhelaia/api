400d59e31d7c1b91e41f71ba4595e3fd08971c56
ditokhelaia <ditokhelaia@gmail.com>
2018-11-21 20:20:27 +0400

    Created ReviewResource and Product Paginate

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    public function product(){
+    public function product()
+    {
         return $this->belongsTo(Product::class);
     }
 }
